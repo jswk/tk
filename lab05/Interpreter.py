cf46126835490795a6eee44ba17227d055aa87f8
Rafał Cymerys <rafal.cym@gmail.com>
2014-01-21 17:30:51 +0100

    Added stub for funcall

--- lab05/Interpreter.py
+++ lab05/Interpreter.py
@@ -8,6 +8,29 @@ from visit import *
 
 class Interpreter(object):
 
+    binary_operations = {
+        '+': (lambda x, y: x + y),
+        '-': (lambda x, y: x - y),
+        '*': (lambda x, y: x * y),
+        '/': (lambda x, y: x / y),
+        '<': (lambda x, y: 1 if x < y else 0),
+        '<=': (lambda x, y: 1 if x <= y else 0),
+        '==': (lambda x, y: 1 if x == y else 0),
+        '>': (lambda x, y: 1 if x > y else 0),
+        '>=': (lambda x, y: 1 if x >= y else 0),
+        '!=': (lambda x, y: 1 if x != y else 0),
+        '&&': (lambda x, y: 1 if x and y else 0),
+        '||': (lambda x, y: 1 if x or y else 0),
+        '<<': (lambda x, y: x << y),
+        '>>': (lambda x, y: x >> y),
+        '|': (lambda x, y: x | y),
+        '&': (lambda x, y: x & y),
+        '^': (lambda x, y: x ^ y),
+        '%': (lambda x, y: x % y)
+    }
+
+    def __init__(self):
+        self.memory_stack = MemoryStack(Memory('global'))
 
     @on('node')
     def visit(self, node):
@@ -15,22 +38,39 @@ class Interpreter(object):
 
     @when(AST.BinOp)
     def visit(self, node):
-        r1 = node.left.accept(self)
-        r2 = node.right.accept(self)
-        # try sth smarter than:
-        # if(node.op=='+') return r1+r2
-        # elsif(node.op=='-') ...
+        r1 = node.left.accept666(self)
+        r2 = node.right.accept666(self)
 
-    @when(AST.RelOp)
+        return Interpreter.binary_operations[node.op](r1, r2)
+
+    @when(AST.CompoundInstruction)
     def visit(self, node):
-        r1 = node.left.accept(self)
-        r2 = node.right.accept(self)
-        # ...
+        for declaration in node.decl:
+            declaration.accept666(self)
+        for instruction in node.instr:
+            instruction.accept666(self)
 
     @when(AST.Assignment)
     def visit(self, node):
-    #
-    #
+        value = node.expr.accept666(self)
+        self.memory_stack.put(node.id.name, value)
+
+    @when(AST.Fundef)
+    def visit(self, node):
+        self.memory_stack.put(node.name, node)
+
+    @when(AST.Funcall)
+    def visit(self, node):
+        function = self.memory_stack.get(node.name.name)
+        memory = Memory(function.name)
+        self.memory_stack.push(memory)
+        # evaluate argument values
+        # zip reduce ftw
+        pairs = zip(function.arguments, node.args)
+        for pair in pairs:
+            name = pair[0].id
+            value = pair[1]. # TODO START HERE
+        self.memory_stack.pop()
 
     @when(AST.Const)
     def visit(self, node):
